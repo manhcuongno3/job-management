64048f56fb5d0407b855ce05126c793e0c1b2360
CuongPC <manhcuongno3@gmail.com>
2024-01-18 18:02:53 +0700

    add basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import Button from '@mui/material/Button'
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
@@ -12,6 +7,7 @@ import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 import Box from '@mui/system/Box'
+import Container from '@mui/material/Container'
 
 function SelectSmall () {
   const { mode, setMode } = useColorScheme()
@@ -49,45 +45,43 @@ function SelectSmall () {
   )
 }
 
-function ModeToggle () {
-  const { mode, setMode } = useColorScheme()
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
-
 function App () {
   return (
-    <>
-      <SelectSmall />
-      <hr />
-      <ModeToggle />
-      <hr />
-      <div>Cuongpc</div>
-      <Typography variant='body2' color='text.secondary'>
-        Test
-      </Typography>
-      <Button variant='text'>Text</Button>
-      <Button variant='contained'>Contained</Button>
-      <Button variant='outlined'>Outlined</Button>
-      <br />
-      <AccessAlarm />
-      <ThreeDRotation />
-      <HomeIcon />
-      <HomeIcon color='primary' />
-      <HomeIcon color='secondary' />
-      <HomeIcon color='success' />
-      <HomeIcon color='action' />
-      <HomeIcon color='disabled' />
-      <HomeIcon sx={{ color: pink[500] }} />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.jm.appBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <SelectSmall />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.dark',
+          width: '100%',
+          height: (theme) => theme.jm.boardBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Board bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.main',
+          width: '100%',
+          height:(theme) => `calc(100vh - ${theme.jm.appBarHeight} - ${theme.jm.boardBarHeight})`,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Boarc content
+      </Box>
+    </Container>
   )
 }
 
--- src/theme.js
+++ src/theme.js
@@ -3,18 +3,24 @@ import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  jm:{
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
         secodary: deepOrange
-      }
+      },
+      spacing: factor => `${0.25 * factor}rem`
     },
     dark: {
       palette: {
         primary: cyan,
         secodary: orange
-      }
+      },
+      spacing: factor => `${0.25 * factor}rem`
     }
   }
   // ...other properties
