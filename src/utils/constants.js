77810b85f388b89b7a094a9437bdc0f14a51a762
CuongPC <manhcuongno3@gmail.com>
2024-02-07 08:53:33 +0700

    fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,13 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://jm-api-7t5n.onrender.com'
+let apiRoot = ''
+
+// console.log('import.meta.env:',import.meta.env);
+// console.log('process.env',process.env);
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://jm-api-7t5n.onrender.com'
+}
+
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //config use process.env to get data from pakeage.json, default is import.meta.env
+  define:{
+    'process.env':process.env
+  },
   plugins: [
     react(),
     svgr()
